2787add059e9c2bb9db69c0f88251df754251b00
Ернар <230709@astanait.edu.kz>
2024-04-23 13:55:44 +0500

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,26 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
         for(int i = 0; i < n; i++) {
-            int k = sc.nextInt();
-            ArrayList<Integer> list1 = new ArrayList<>();
-            for(int j = 0; j < k; j++) {
-                int l = sc.nextInt();
-                list1.add(l);
-            }
-            list.add(list1);
+            int l = sc.nextInt();
+            list.add(l);
         }
-        int numOfSelections = sc.nextInt();
-        for(int i = 0; i < numOfSelections; i++) {
-            int x = sc.nextInt() - 1;
-            int y = sc.nextInt() - 1;
-            if (x >= 0 && x < list.size() && y >= 0 && y < list.get(x).size()) {
-                System.out.println(list.get(x).get(y));
-            } else {
-                System.out.println("ERROR");
+        int Q = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i< Q; i++) {
+            String operation = sc.nextLine();
+            if(operation.equals("Insert")) {
+                int x = sc.nextInt();
+                int value = sc.nextInt();
+                list.add(x, value);
+                sc.nextLine();
+            }
+            else if(operation.equals("Delete")) {
+                int x = sc.nextInt();
+                list.remove(x);
+                sc.nextLine();
             }
         }
+        System.out.println(list);
     }
 
 
