80c631fa12f23dded8bf4803ffefb663ea5555dd
Ернар <230709@astanait.edu.kz>
2024-04-23 23:37:38 +0500

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,34 @@
 package org.example;
-import java.lang.reflect.Array;
+
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] numbers = input.split(" ");
-        ArrayList<Integer> sequence = new ArrayList<>();
-        for (String num : numbers) {
-            sequence.add(Integer.parseInt(num));
+        int N = scanner.nextInt();
+
+        int[] sequence = new int[2 * N];
+        int left = N - 1, right = N;
+
+        for (int i = 0; i < N; i++) {
+            int operation = scanner.nextInt();
+
+            if (operation == 1 || operation == 2){
+                int disk = scanner.nextInt();
+                if (operation == 1) {
+                    sequence[left--] = disk;
+                } else {
+                    sequence[right++] = disk;
+                }
+            }
+            else{
+                if (operation == 3) {
+                    System.out.print(sequence[++left] + " ");
+                }
+                else {
+                    System.out.print(sequence[--right] + " ");
+                }
+            }
         }
-        reverseSequence(sequence, 0, sequence.size() - 1);
-        for (int i = 0; i < sequence.size(); i++) {
-            System.out.print(sequence.get(i) + " ");
-        }
-    }
-    public static void reverseSequence(ArrayList<Integer> sequence, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-        int temp = sequence.get(start);
-        sequence.set(start, sequence.get(end));
-        sequence.set(end, temp);
-        reverseSequence(sequence, start + 1, end - 1);
     }
 }
