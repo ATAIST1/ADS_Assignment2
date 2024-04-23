846bfe17f58d727968eb28b471902aa9ede28d7f
Ернар <230709@astanait.edu.kz>
2024-04-23 23:55:52 +0500

    6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,39 @@
 package org.example;
 public class Main {
 
-    public static int unableToEat(int[] students, int[] samsas) {
-        int unableToEatCount = 0;
-        int currentStudentIndex = 0;
-        for (int samsa : samsas) {
-            if (currentStudentIndex >= students.length) {
-                break;
-            }
-            int studentPreference = students[currentStudentIndex];
-            if (studentPreference == samsa) {
-                currentStudentIndex++;
-            } else {
-                unableToEatCount++;
-            }
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int leftChildIdx = 2 * i + 1;
+        int rightChildIdx = 2 * i + 2;
+
+        // If left child is larger than root
+        if (leftChildIdx < n && arr[leftChildIdx] > arr[largest]) {
+            largest = leftChildIdx;
         }
-        return unableToEatCount;
+        if (rightChildIdx < n && arr[rightChildIdx] > arr[largest]) {
+            largest = rightChildIdx;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
     }
 
     public static void main(String[] args) {
-        int[] students = {1, 1, 0, 0};
-        int[] samsas = {0, 1, 0, 1};
-        System.out.println(unableToEat(students, samsas));
+        int[] arr = {4, 10, 3, 5, 1};
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        System.out.println("Max heap:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
